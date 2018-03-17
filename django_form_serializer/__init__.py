721aed70e1814c5432e16d80de8ab99d26c6a5bf
Haris Ibrahim K. V <blucalvin@gmail.com>
2018-03-17 17:16:33 +0530

    Initial commit

---
+++ django_form_serializer/__init__.py
@@ -0,0 +1,86 @@
+class DjangoFormSerializer(object):
+
+    def get_fieldsets(self, fieldsets):
+        result = []
+        for item in fieldsets:
+            fieldset = {}
+            title = item[0]
+            fields = item[1]['fields'] if type(
+                item[1]['fields']) == tuple else (item[1]['fields'],)
+            fieldset['title'] = title
+            fieldset['fields'] = fields
+            result.append(fieldset)
+        return result
+
+    def get_choices(self, queryset):
+        return [
+            (str(instance.id), str(instance)) for instance in queryset.all()
+        ]
+
+    def get_initial_value(self, key, form):
+        form_field = form.fields[key]
+        field_type = form_field.__class__.__name__
+        initial_value = None
+        if field_type == 'ImageField':
+            try:
+                initial_value = form.initial[key].url
+            except:
+                initial_value = None
+        elif field_type == 'LazyTypedChoiceField':
+            initial_value = form.initial.get(key).code if form.initial.get(
+                key) else None
+        elif field_type == 'TagField':
+            if form.initial[key]:
+                initial_value = list(form.initial[key].values(
+                    'tag__slug', 'tag__name'))
+        elif field_type in ['TypedChoiceField', 'BooleanField']:
+            initial_value = form.initial.get(key)
+            return initial_value
+        else:
+            initial_value = str(
+                form.initial.get(key)) if form.initial.get(key) else None
+        return (initial_value if initial_value else '')
+
+    def get_placeholder(self, key, form):
+        if key == 'tags':
+            return ''
+        elif form.fields[key].widget.attrs.get('placeholder'):
+            return form.fields[key].widget.attrs['placeholder']
+        else:
+            return form.fields[key].help_text
+
+    def parse(self, form):
+        result = {}
+        result['fields'] = {}
+        result['fieldsets'] = self.get_fieldsets(form.Meta.fieldsets)
+        keys = form.fields.keys()
+
+        for key in keys:
+            input_element = {}
+            choices = None
+            value = None
+            disabled = False
+
+            disabled = form.fields[key].disabled
+            django_input_type = form.fields[key].widget.__class__.__name__
+            if django_input_type in ['RadioSelect', 'LazySelect', 'Select']:
+                if type(form.fields[key].choices) == list:
+                    choices = form.fields[key].choices
+                else:  # 'Select'
+                    queryset = form.fields[key].queryset
+                    choices = self.get_choices(queryset)
+
+            value = self.get_initial_value(key, form)
+            label = form.fields[key].label
+            placeholder = self.get_placeholder(key, form)
+            if key == 'tags':
+                label = "Tags"
+
+            input_element['type'] = django_input_type
+            input_element['choices'] = choices
+            input_element['value'] = value
+            input_element['label'] = label
+            input_element['placeholder'] = placeholder
+            input_element['disabled'] = disabled
+            result['fields'][key] = input_element
+        return result
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django_form_serializer',
+      version='0.1',
+      description='Converts and exposes Django Forms as JSONs',
+      url='https://github.com/bitlabstudio/django-form-serializer',
+      author='Bitmazk',
+      author_email='info@bitmazk.com',
+      license='MIT',
+      packages=['django_form_serializer'],
+      zip_safe=False)
